bf04abecc49c7f2cb9a6c521bb5e1cb501e6bdc8
smartgamer <htchd7ji@live.com>
2019-01-11 10:53:20 -0500

    c

--- fruitImageClassKeras.R
+++ fruitImageClassKeras.R
@@ -1,4 +1,4 @@
-# Image classification with keras in roughly 100 lines of code.
+# Image classification with keras and explain models with lime
 
 #https://shirinsplayground.netlify.com/2018/06/keras_fruits/
 #https://www.kaggle.com/moltean/fruits/data
@@ -171,8 +171,133 @@ sessionInfo()
 ### Explaining Keras image classification models with lime ###
 #https://shirinsplayground.netlify.com/2018/06/keras_fruits_lime/
 # Thomas wrote a very nice article about how to use keras and lime in R! Here, I am following this article to use Imagenet (VGG16) to make and explain predictions of fruit images and then I am extending the analysis to last week’s model and compare it with the pretrained net.
+# install.packages("keras")
+# install.packages("lime")
+# install.packages("magick")
+library(keras)   # for working with neural nets
+library(lime)    # for explaining models
+library(magick)  # for preprocessing images
+library(ggplot2) # for additional plotting
+# Loading the pretrained Imagenet model
+model <- application_vgg16(weights = "imagenet", include_top = TRUE)
+model
+
+# loading my own model from last week’s post
+model2 <- load_model_hdf5(filepath = "/Users/shiringlander/Documents/Github/DL_AI/Tutti_Frutti/fruits-360/keras/fruits_checkpoints.h5")
+model2
+
+# Load and prepare images #
+# Here, I am loading and preprocessing two images of fruits (and yes, I am cheating a bit because I am choosing images where I expect my model to work as they are similar to the training images…).
+
+# Banana
+test_image_files_path <- "/Users/shiringlander/Documents/Github/DL_AI/Tutti_Frutti/fruits-360/Test"
+
+img <- image_read('https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/272px-Banana-Single.jpg')
+img_path <- file.path(test_image_files_path, "Banana", 'banana.jpg')
+image_write(img, img_path)
+#plot(as.raster(img))
+Clementine
+img2 <- image_read('https://cdn.pixabay.com/photo/2010/12/13/09/51/clementine-1792_1280.jpg')
+img_path2 <- file.path(test_image_files_path, "Clementine", 'clementine.jpg')
+image_write(img2, img_path2)
+#plot(as.raster(img2))
 
 
+# Superpixels #
+
+plot_superpixels(img_path, n_superpixels = 35, weight = 10)
+plot_superpixels(img_path2, n_superpixels = 50, weight = 20)
+#From the superpixel plots we can see that the clementine image has a higher resolution than the banana image.
+
+# Prepare images for Imagenet #
+image_prep <- function(x) {
+  arrays <- lapply(x, function(path) {
+    img <- image_load(path, target_size = c(224,224))
+    x <- image_to_array(img)
+    x <- array_reshape(x, c(1, dim(x)))
+    x <- imagenet_preprocess_input(x)
+  })
+  do.call(abind::abind, c(arrays, list(along = 1)))
+}
+# test predictions
+res <- predict(model, image_prep(c(img_path, img_path2)))
+imagenet_decode_predictions(res)
+
+# load labels and train explainer
+model_labels <- readRDS(system.file('extdata', 'imagenet_labels.rds', package = 'lime'))
+explainer <- lime(c(img_path, img_path2), as_classifier(model, model_labels), image_prep)
+# Training the explainer (explain() function) can take pretty long. It will be much faster with the smaller images in my own model but with the bigger Imagenet it takes a few minutes to run.
+
+explanation <- explain(c(img_path, img_path2), explainer, 
+                       n_labels = 2, n_features = 35,
+                       n_superpixels = 35, weight = 10,
+                       background = "white")
+# plot_image_explanation() only supports showing one case at a time
+plot_image_explanation(explanation)
+
+clementine <- explanation[explanation$case == "clementine.jpg",]
+plot_image_explanation(clementine)
+
+# Prepare images for my own model
+# test predictions (analogous to training and validation images)
+test_datagen <- image_data_generator(rescale = 1/255)
+
+test_generator <- flow_images_from_directory(
+  test_image_files_path,
+  test_datagen,
+  target_size = c(20, 20),
+  class_mode = 'categorical')
+
+predictions <- as.data.frame(predict_generator(model2, test_generator, steps = 1))
+
+load("/Users/shiringlander/Documents/Github/DL_AI/Tutti_Frutti/fruits-360/fruits_classes_indices.RData")
+fruits_classes_indices_df <- data.frame(indices = unlist(fruits_classes_indices))
+fruits_classes_indices_df <- fruits_classes_indices_df[order(fruits_classes_indices_df$indices), , drop = FALSE]
+colnames(predictions) <- rownames(fruits_classes_indices_df)
+
+t(round(predictions, digits = 2))
+
+for (i in 1:nrow(predictions)) {
+  cat(i, ":")
+  print(unlist(which.max(predictions[i, ])))
+}
+
+# This seems to be incompatible with lime, though (or if someone knows how it works, please let me know) - so I prepared the images similarly to the Imagenet images.
+
+image_prep2 <- function(x) {
+  arrays <- lapply(x, function(path) {
+    img <- image_load(path, target_size = c(20, 20))
+    x <- image_to_array(img)
+    x <- reticulate::array_reshape(x, c(1, dim(x)))
+    x <- x / 255
+  })
+  do.call(abind::abind, c(arrays, list(along = 1)))
+}
+# prepare labels
+fruits_classes_indices_l <- rownames(fruits_classes_indices_df)
+names(fruits_classes_indices_l) <- unlist(fruits_classes_indices)
+fruits_classes_indices_l
+
+# train explainer
+explainer2 <- lime(c(img_path, img_path2), as_classifier(model2, fruits_classes_indices_l), image_prep2)
+explanation2 <- explain(c(img_path, img_path2), explainer2, 
+                        n_labels = 1, n_features = 20,
+                        n_superpixels = 35, weight = 10,
+                        background = "white")
+# plot feature weights to find a good threshold for plotting block (see below)
+explanation2 %>%
+  ggplot(aes(x = feature_weight)) +
+  facet_wrap(~ case, scales = "free") +
+  geom_density()
+
+
+# plot predictions
+plot_image_explanation(explanation2, display = 'block', threshold = 5e-07)
+
+clementine2 <- explanation2[explanation2$case == "clementine.jpg",]
+plot_image_explanation(clementine2, display = 'block', threshold = 0.16)
+
+sessionInfo()
 
 
 
